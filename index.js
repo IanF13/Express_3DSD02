4220a13ffe574432691042e783fd15bdea896fbc
Ian_Do_041 <130380548+IanF13@users.noreply.github.com>
2024-10-04 11:38:33 +0000

    Criação da rota 404 e inicio do menu

--- index.js
+++ index.js
@@ -2,9 +2,16 @@ const express = require("express");
 const app = express();
 
 const PORT = 8081;
+//Criação das Rotas
+app.use(express.static(__dirname + "public"));
 
-app.get("/", function (req, res) {
-  res.send("Mauricio de Souza");
+
+app.get("/sobre", function (req, res) {
+  res.sendFile(__dirname + "/src/views/about.html");
+});
+
+app.use(function (req, res) {
+  res.status(404).sendFile(__dirname + "/src/views/404.html");
 });
 
 app.listen(PORT, () => {
